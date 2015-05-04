d2ff6bb1fb977b7a9bafc9b8072f8390142b07c9
Daniel Yak <danielyaa5@gmail.com>
2015-05-04 19:49:09 -0400

    updated description list ui, table instead of ul

--- meanjs/public/modules/rfqs/controllers/rfqs.client.controller.js
+++ meanjs/public/modules/rfqs/controllers/rfqs.client.controller.js
@@ -76,57 +76,58 @@ angular.module('rfqs').controller('RfqsController', ['$scope', '$stateParams', '
             }
         };
 
-        // Init array of material descriptions for edit-rfq page 
-        $scope.initEditRfq = function() {
-
-        };
-
         $scope.addToDescriptionListOnEnter = function(keyEvent) {
             if (keyEvent.which === 13) {
                 $scope.addToDescriptionList();
             }
         };
 
-        $scope.removeFromDescriptionList = function(listItem) {
-            $scope.materialDescriptionArray.splice($scope.materialDescriptionArray.indexOf(listItem), 1);
+        $scope.removeDescription = function(descriptionItem) {
+            $scope.materialDescriptionArray.splice($scope.materialDescriptionArray.indexOf(descriptionItem), 1);
         };
 
         //Handles the generation of list of user inputed descriptions 
         $scope.addToDescriptionList = function() {
             var material = {};
-            var descriptor;
+            var name;
             var value;
 
-            if (this.materialDescriptor !== undefined && this.materialValue !== undefined) {
-                descriptor = this.materialDescriptor.trim();
-                value = this.materialValue.trim();
+            if (this.descriptionName !== undefined && this.descriptionValue !== undefined) {
+                name = this.descriptionName.trim();
+                value = this.descriptionValue.trim();
 
-                if (descriptor.length !== 0 && this.materialValue.length !== 0) {
+                if (name.length !== 0 && this.descriptionValue.length !== 0) {
                     material = {
-                        descriptor: descriptor.charAt(0).toUpperCase() + descriptor.slice(1),
+                        name: name.charAt(0).toUpperCase() + name.slice(1),
                         value: value
                     };
                     $scope.materialDescriptionArray.push(material);
-                    this.materialDescriptor = '';
-                    this.materialValue = '';
+                    this.descriptionName = '';
+                    this.descriptionValue = '';
                     $scope.descriptionError = null;
-                    document.getElementById('materialDescriptor').focus();
+                    document.getElementById('descriptionName').focus();
                 } else {
-                    if (descriptor.length === 0 && value.length === 0) {
+                    if (name.length === 0 && value.length === 0) {
                         $scope.descriptionError = 'Enter a descriptor and value';
-                    } else if (descriptor.length === 0) {
+                        document.getElementById('descriptionName').focus();
+                    } else if (name.length === 0) {
                         $scope.descriptionError = 'Enter a descriptor';
+                        document.getElementById('descriptionName').focus();
                     } else {
                         $scope.descriptionError = 'Enter a value';
+                        document.getElementById('descriptionValue').focus();
                     }
                 }
             } else {
-                if (this.materialDescriptor === undefined && this.materialValue === undefined) {
+                if (this.descriptionName === undefined && this.descriptionName === undefined) {
                     $scope.descriptionError = 'Enter a descriptor and value';
-                } else if (this.materialDescriptor === undefined) {
+                    document.getElementById('descriptionName').focus();
+                } else if (this.descriptionName === undefined) {
                     $scope.descriptionError = 'Enter a descriptor';
+                    document.getElementById('descriptionName').focus();
                 } else {
                     $scope.descriptionError = 'Enter a value';
+                    document.getElementById('descriptionValue').focus();
                 }
             }
         };
