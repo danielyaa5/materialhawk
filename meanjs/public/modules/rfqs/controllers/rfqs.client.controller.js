0fa3bebc6968e7e54e722f8d443f16c36af88e2e
Daniel Yak <danielyaa5@gmail.com>
2015-05-24 12:32:11 -0400

    added angular-ui datepicker instead of html5 datepicker

--- meanjs/public/modules/rfqs/controllers/rfqs.client.controller.js
+++ meanjs/public/modules/rfqs/controllers/rfqs.client.controller.js
@@ -7,6 +7,62 @@ angular.module('rfqs').controller('RfqsController', ['$scope', '$http', '$stateP
         $scope.materialObjects = [];
         $scope.amlTags = [];
 
+        //Angular UI Datepicker
+
+        $scope.clear = function() {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+        };
+
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date();
+        afterTomorrow.setDate(tomorrow.getDate() + 2);
+        $scope.events = [{
+            date: tomorrow,
+            status: 'full'
+        }, {
+            date: afterTomorrow,
+            status: 'partially'
+        }];
+
+        $scope.getDayClass = function(date, mode) {
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+                for (var i = 0; i < $scope.events.length; i++) {
+                    var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                    if (dayToCheck === currentDay) {
+                        return $scope.events[i].status;
+                    }
+                }
+            }
+
+            return '';
+        };
+
+        //Get JSON file for metals
         $http.get('modules/rfqs/resources/metals.json').success(function(data) {
             $scope.data = data;
         });
@@ -80,7 +136,7 @@ angular.module('rfqs').controller('RfqsController', ['$scope', '$http', '$stateP
             // Create new Rfq object
             var rfq = new Rfqs({
                 nickname: this.nickname,
-                completeBy: this.completeBy,
+                completeBy: this.dt,
                 quoteType: this.quoteType,
                 notes: this.notes,
                 privacy: this.privacy,
